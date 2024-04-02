109d43943c13d4ba76254444997c0c4b18209495
Vitaliy Viktorovich <po4tavela@gmail.com>
2024-04-02 14:29:15 +0300

    Added collection view for overview trainings

--- WorkoutApp/Presentation/Overview/OverviewController.swift
+++ WorkoutApp/Presentation/Overview/OverviewController.swift
@@ -10,6 +10,16 @@ import UIKit
 final class OverviewController: BaseController {
     // MARK: Private properties
     private let navBar = OverviewNavBar()
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = .zero
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.showsVerticalScrollIndicator = false
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    private var dataSource: [TrainingData] = []
     
     // MARK: Lifecycle
     override func viewDidLoad() {
@@ -31,7 +41,10 @@ extension OverviewController {
     override func setupViews() {
         super.setupViews()
         
-        view.addViews(navBar)
+        view.addViews(
+            navBar,
+            collectionView
+        )
     }
     
     override func setupConstraints() {
@@ -41,6 +54,11 @@ extension OverviewController {
             navBar.topAnchor.constraint(equalTo: view.topAnchor),
             navBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             navBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: navBar.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Constants.Common.horizontalPadding),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Constants.Common.horizontalPadding),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
     
@@ -48,6 +66,19 @@ extension OverviewController {
         super.configureAppearance()
         
         navigationController?.navigationBar.isHidden = true
+        
+        collectionView.register(TrainingCell.self, forCellWithReuseIdentifier: TrainingCell.id)
+        collectionView.register(
+            SectionHeader.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: SectionHeader.id
+        )
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        dataSource = MockData.shared.trainingData
+        collectionView.reloadData()
     }
 }
 
@@ -59,5 +90,81 @@ extension OverviewController {
     
     func addButtonAction() {
         print("Add button tapped")
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension OverviewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: Constants.Overview.trainingCellHeight)
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        referenceSizeForHeaderInSection section: Int
+    ) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: Constants.Overview.cellHeaderHeight)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        dataSource.count
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension OverviewController: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        dataSource[section].items.count
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: TrainingCell.id,
+            for: indexPath
+        )  as? TrainingCell else { return .init() }
+        
+        let item = dataSource[indexPath.section].items[indexPath.row]
+        
+        let roundedType: CellRoundedType
+        
+        if indexPath.row == 0 && indexPath.row == dataSource[indexPath.section].items.count - 1 {
+            roundedType = .all
+        } else if indexPath.row == 0 {
+            roundedType = .top
+        } else if indexPath.row == dataSource[indexPath.section].items.count - 1 {
+            roundedType = .bottom
+        } else {
+            roundedType = .notRounded
+        }
+        
+        cell.configure(with: item.title, subtitle: item.subtitle, isDone: item.isDone, roundedType)
+        
+        return cell
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        viewForSupplementaryElementOfKind kind: String,
+        at indexPath: IndexPath
+    ) -> UICollectionReusableView {
+        guard let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: SectionHeader.id,
+            for: indexPath
+        ) as? SectionHeader else { return .init() }
+        
+        header.configure(with: dataSource[indexPath.section].date)
+        return header
     }
 }
